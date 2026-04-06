34ad6f85bddae4a11865bc8e60aab8f7f4d8a7c5
UltimateSlime <2750.create@gmail.com>
2026-04-06 21:57:20 +0900

    feat: working 3d scene with physics

--- src/world.rs
+++ src/world.rs
@@ -4,42 +4,26 @@ use bevy::render::render_resource::{TextureViewDescriptor, TextureViewDimension}
 use bevy_flycam::prelude::*;
 use avian3d::prelude::*;
 
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(NoCameraPlayerPlugin)  // PlayerPluginの代わり、カメラは自分でspawnする
-        .add_plugins(PhysicsPlugins::default())
-        .add_systems(Startup, setup)
-        .add_systems(Update, asset_loaded)
-        .run();
-}
-
 #[derive(Resource)]
-struct Skyboxhandle {
-    image: Handle<Image>,
-    is_loaded: bool,
+pub struct Skyboxhandle {
+    pub image: Handle<Image>,
+    pub is_loaded: bool,
 }
 
-fn setup(
+// 地面・建物・ライトのspawn
+pub fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     asset_server: Res<AssetServer>,
 ) {
     let skybox_handle: Handle<Image> = asset_server.load("textures/Ryfjallet_cubemap.png");
-    commands.insert_resource(Skyboxhandle {
+    commands.insert_resource(SkyboxHandle {
         image: skybox_handle.clone(),
         is_loaded: false,
     });
 
-    // カメラ (FlyCamコンポーネントを付けることでWASD操作可能に)
-    commands.spawn((
-        Camera3d::default(),
-        Transform::from_xyz(0.0, 2.0, 8.0).looking_at(Vec3::ZERO, Vec3::Y),
-        FlyCam,
-    ));
-
-    //　太陽光
+    // 太陽光
     commands.spawn((
         DirectionalLight {
             illuminance: 10000.0,
@@ -56,13 +40,6 @@ fn setup(
         Transform::from_xyz(0.0, 0.0, 0.0),
         RigidBody::Static,
         Collider::cuboid(50.0, 0.1, 50.0),
-    ));
-
-    // 箱
-    commands.spawn((
-        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
-        MeshMaterial3d(materials.add(Color::srgb(0.5, 0.8, 0.5))),
-        Transform::from_xyz(0.0, 0.5, 0.0),
     ));
 
     // 箱を複数配置
@@ -91,20 +68,19 @@ fn setup(
         RigidBody::Dynamic,  // 動的な剛体として設定
         Collider::cuboid(0.9, 0.9, 0.9), // コライダーを追加
     ));
-
 }
 
-fn asset_loaded(
+// Skybox非同期ロード
+pub fn asset_loaded(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut images: ResMut<Assets<Image>>,
-    mut skybox_res: ResMut<Skyboxhandle>,
+    mut skybox_res: ResMut<SkyboxHandle>,
     camera_query: Query<Entity, With<Camera3d>>,
 ) {
-    if skybox_res.is_loaded { return;}
-    if !asset_server.load_state(&skybox_res.image).is_loaded() { return;}
-
-
+    if skybox_res.is_loaded { return; }  // すでにロードされている場合は何もしない
+    if !asset_server.load_sate(&skybox_res.image).is_loaded() { return; }  // まだロードされていない場合は何もしない
+    
     let image = images.get_mut(&skybox_res.image).unwrap();
     if image.texture_descriptor.array_layer_count() == 1 {
         let layers = image.height() / image.width();
@@ -125,5 +101,4 @@ fn asset_loaded(
     }
 
     skybox_res.is_loaded = true;
-
 }
