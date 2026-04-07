d151227b37b02a3f89ba8f4a40076671f870e017
UltimateSlime <2750.create@gmail.com>
2026-04-08 00:31:14 +0900

    feat: add TPS/FPS camera switch, mouse look, and camera collision

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,31 @@ use avian3d::prelude::*;
 mod world;
 mod player;
 
+fn close_on_esc(
+    keyboad: Res<ButtonInput<KeyCode>>,
+    mut exit: MessageWriter<AppExit>,
+) {
+    if keyboad.just_pressed(KeyCode::Escape) {
+        exit.write(AppExit::Success);
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(NoCameraPlayerPlugin)  // PlayerPluginの代わり、カメラは自分でspawnする
         .add_plugins(PhysicsPlugins::default())
-        .add_systems(Startup, (world::setup, player::spawn_player))
-        .add_systems(Update, (world::asset_loaded, player::move_player))
-        .add_systems(Update, player::camera_follow)
+        .insert_resource(player::CameraMode::TPS)
+        .insert_resource(player::CameraAngle::default()) 
+        .add_systems(Startup, (world::setup, player::spawn_player))    
+        .add_systems(Update, (
+            world::asset_loaded,
+            player::switch_camera_mode,
+            player::mouse_look,
+            player::move_player,
+            player::camera_follow,
+            close_on_esc,
+        ))
         .run();
 }
+
