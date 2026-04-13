8acecd3a196a9f0175a6524d31119a3ae49167bd
UltimateSlime <2750.create@gmail.com>
2026-04-14 00:24:09 +0900

    wip: crouch collider offset investigation

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,7 @@ fn main() {
                 world::asset_loaded,
                 camera::update_camera,
                 player::move_player,
+                player::update_player_collider,
                 player::update_player_model_offset,
                 player::update_animation,
                 camera::camera_follow,
